a10e349e1c0db600cac0aabfab1b6f9ecafbfbaa
sirova <mohit.sirova@girnarsoft.co.in>
2023-06-07 19:10:27 +0530

    API

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var app = express();
+const hostname = '0.0.0.0';
 app.use(bodyParser.json());
 app.use(cors({
     origin: '*'
 }));
 global.DB = require('./config/connection');
-app.listen(8080, function(err) {
+app.listen(8080,hostname, function(err) {
     if(err){
        console.log(err);
        } else {
